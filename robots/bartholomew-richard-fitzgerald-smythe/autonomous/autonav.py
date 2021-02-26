f054f0cd1023b159507e20705bbefb687545573f
Nyl0k <colin.s.frazer@gmail.com>
2021-02-26 07:51:32 -0500

    Fixed internodal distances

--- robots/bartholomew-richard-fitzgerald-smythe/autonomous/autonav.py
+++ robots/bartholomew-richard-fitzgerald-smythe/autonomous/autonav.py
@@ -40,16 +40,12 @@ class Autonav(AutonomousStateMachine):
         for i in range(len(out[0])):
             traj_pts.append([out[0][i], out[1][i]])
 
-        self.angles = []
-        self.distance = 0
+        #Tuples: angle to turn to and distance to drive (angle, dist)
+        self.nodes = []
         prev_pt = traj_pts[0]
         for pt in traj_pts[1:]:
-            self.angles.append(self.calc_angle(prev_pt, pt))
-            self.distance += math.sqrt((pt[0]-prev_pt[0])+(pt[1]-prev_pt[1]))
+            self.nodes.append([self.calc_angle(prev_pt, pt), math.dist(prev_pt, pt)])
             prev_pt = pt
-
-        self.distance /= resolution
-        self.spacing = abs(self.distance) #This is just to copy by value rather than reference, abs isn't mathematically important
 
         self.next_state('track')
     
@@ -57,11 +53,11 @@ class Autonav(AutonomousStateMachine):
     def track(self):
         index = 0
         self.drivetrain.powertrain.set_arcade_powers(power=0.1)
-        while index < len(self.angles):
-            self.drivetrain.turn_to_angle(self.angles[index])
-            if self.drivetrain.get_position() > self.distance
+        while index < len(self.nodes):
+            self.drivetrain.turn_to_angle(self.nodes[index][0])
+            if self.drivetrain.get_position(EncoderSide.BOTH) > self.nodes[index][1]
                 index+=1
-                self.distance+=self.spacing
+                self.drivetrain.encoders.reset()
         while not self.drivetrain.turn_pid.get_on_target():
             continue
         self.drivetrain.stop()
